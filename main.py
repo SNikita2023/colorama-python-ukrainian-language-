6ed7b6f6844ff06f2a859f10d8718aa2711a03ea
SNikita2023 <118457982+SNikita2023@users.noreply.github.com>
2022-11-17 16:12:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#з допомогою цих команд потрібно встановити бібліотеку
+#py -m pip install colorama
+#или
+#python -m pip install colorama
+
+
+#from colorama import init
+#init()
+#from colorama import Fore, Back, Style
+#print(Fore.GREEN + 'значення Fore - робить колір тексту (GREEN -колір тексту зелений)')
+#print(Back.YELLOW + 'значення BACK - робить колір фону текста (YELLOW -колір фону текста жовтий)')
+#print(Style.BRIGHT + 'значення BRIGHT - робить текст ярче' + Style.RESET_ALL)
+
+# значення FORE
+# BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, (RESET - сброс)
+# значення BACK
+# BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, (RESET - сброс)
+# значення STYLE
+#DIM, NORMAL, BRIGHT, (RESET_ALL  - сбросить усе)
+
